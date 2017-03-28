26ef6bef38492aa53468ba84e6d5c728d1a53a65
Jeasonweb <1072486871@qq.com>
2017-03-28 11:15:35 +0800

    ajax请求数据出现了问题，暂时不知道是什么情况，待处理

---
+++ src/AjaxForJSX.js
@@ -0,0 +1,39 @@
+var UserGist = React.createClass({
+	getInitialState:function(){
+		return {
+			userName:"",
+			lastUserGist:''			
+		}
+		
+	},
+	componentDidMount:function(){
+		this.serverRequest = $.get(this.props.source, function (result) {
+			console.log(result);
+		      var lastGist = result[0];
+		      this.setState({
+		        username: lastGist.owner.login,
+		        lastGistUrl: lastGist.html_url
+		      });
+		}.bind(this));		
+	},
+	
+	componentWillUnmount: function() {
+	    this.serverRequest.abort();
+	},
+	
+	
+	render:function(){
+		return (
+		      <div>
+		        {this.state.username} 用户最新的 Gist 共享地址：
+		        <a href={this.state.lastGistUrl}>{this.state.lastGistUrl}</a>
+		      </div>
+			);			
+	}
+	
+});
+
+ReactDOM.render(
+	<UserGist   source="https://api.github.com/users/octocat/gists" />,
+	document.getElementById('root')
+	);
